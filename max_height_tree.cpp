b567eb2bd75e9e62515a8dfcab3dd75e142cf290
Varun Singh <66132670+varunsingh4@users.noreply.github.com>
2026-02-12 23:00:13 -0800

    Implement maxDepth function for binary tree

---
+++ max_height_tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+        {
+            return 0;
+        }
+        int leftHeight=maxDepth(root->left);
+        int rightHeight=maxDepth(root->right);
+        return 1+max(leftHeight,rightHeight);
+
+        
+    }
+};
