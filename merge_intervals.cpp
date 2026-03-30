ca49a73761ab39de041a8404ffc9b925430d2528
Varun Singh <66132670+varunsingh4@users.noreply.github.com>
2026-03-30 00:25:41 -0700

    Implement merge intervals function

---
+++ merge_intervals.cpp
@@ -0,0 +1,20 @@
+
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end());
+
+    vector<vector<int>> ans;
+    vector<int> current = intervals[0];
+
+    for (int i = 1; i < intervals.size(); i++) {
+        if (current[1] >= intervals[i][0]) {
+            current[1] = max(current[1], intervals[i][1]);
+        } else {
+            ans.push_back(current);
+            current = intervals[i];
+        }
+    }
+
+    ans.push_back(current);
+    return ans;
+}
+
